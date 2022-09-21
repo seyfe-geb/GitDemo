55e9fc049eeaf0ea07e44e455dae996e97464537
Seyfegebriel <syberglife@gmail.com>
2022-09-20 20:51:53 -0700

    Added GitDemo to local repo

---
+++ src/main/java/net/seyfe/App.java
@@ -0,0 +1,18 @@
+package net.seyfe;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        print("Bonjour, mon frere!!");
+    }
+
+    public static void print(String message){
+        System.out.println(message);
+    }
+}
